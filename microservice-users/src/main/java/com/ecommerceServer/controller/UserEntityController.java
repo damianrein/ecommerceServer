dda195aa4444f57de2768ac324edfbfc78011163
damianrein <damianrein464@gmail.com>
2024-08-26 20:10:26 -0300

    -

--- microservice-users/src/main/java/com/ecommerceServer/controller/UserEntityController.java
+++ microservice-users/src/main/java/com/ecommerceServer/controller/UserEntityController.java
@@ -1,11 +1,27 @@
 package com.ecommerceServer.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerceServer.model.UserEntity;
 import com.ecommerceServer.service.UserEntityService;
 
 @RestController
+@RequestMapping("/v2")
 public class UserEntityController {
 
 	private UserEntityService service;
+	
+	public UserEntityController(UserEntityService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserEntity>> findAllUsers(){
+		return ResponseEntity.ok(service.getAllUserEntitys());
+	}
 }
--- microservice-users/src/main/java/com/ecommerceServer/service/UserEntityService.java
+++ microservice-users/src/main/java/com/ecommerceServer/service/UserEntityService.java
@@ -1,11 +1,30 @@
 package com.ecommerceServer.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.ecommerceServer.model.UserEntity;
 import com.ecommerceServer.repository.IUserEntityRepository;
 
 @Service
 public class UserEntityService {
 
 	private IUserEntityRepository repo;
+
+	public UserEntityService(IUserEntityRepository repo) {
+		this.repo = repo;
+	}
+	
+	public List<UserEntity> getAllUserEntitys(){
+		return repo.findAll();
+	}
+	
+	public UserEntity getUserEntityByEmail(String email) {
+		return repo.findByEmail(email);
+	}
+	
+	public void deleteUserEntity(Long id) {
+		repo.deleteById(id);
+	}
 }
